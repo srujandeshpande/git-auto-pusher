b7ec599d7c608a92e7fa746b3045952cc09973fc
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-19 17:31:56 +0530

    04/19/2020 16:58:41

--- ps-git-file.ps1
+++ ps-git-file.ps1
@@ -1,4 +1,4 @@
-echo "Enter the name of the file you want to continously track"
+$Server = Read-Host -Prompt 'Enter the name of the file you would like to track'
 $FileName = "file.txt"
 $FileTime = Get-Date
 
---
+++ ps-git-folder.ps1
@@ -0,0 +1,15 @@
+$Server = Read-Host -Prompt 'Enter the name of the file you would like to track'
+$FileName = "file.txt"
+$FileTime = Get-Date
+
+# endless loop
+for () {
+    $file = Get-Item $FileName
+    if ($FileTime -ne $file.LastWriteTime) {
+        git add .
+        git commit -m "$FileTime"
+        git push
+    }
+    $FileTime = $file.LastWriteTime
+    Start-Sleep 1
+}
