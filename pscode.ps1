0284fd8647f75830cfeadc78dc21d8261ca33592
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-19 16:30:31 +0530

    04/19/2020 16:30:31

---
+++ pscode.ps1
@@ -0,0 +1,14 @@
+$FileName = "file.txt"
+$FileTime = Get-Date
+
+# endless loop
+for () {
+    $file = Get-Item $FileName
+    if ($FileTime -ne $file.LastWriteTime) {
+        git add .
+        git commit -m "$FileTime"
+        git push
+    }
+    $FileTime = $file.LastWriteTime
+    Start-Sleep 5
+}
