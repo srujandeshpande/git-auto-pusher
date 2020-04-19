db3032953a005a958bb473eb18bfd6e2b02d2226
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-19 17:57:11 +0530

    auto-commit 04/19/2020 17:57:11

--- ps-git-folder.ps1
+++ ps-git-folder.ps1
@@ -2,7 +2,7 @@ cls
 write-Host "Welcome to git-auto-pusher! This was created by Srujan Deshpande"
 write-Host "This version will track the parent folder"
 $scriptPath = (Get-Location).Path
-$FileName = Split-Path $scriptPath
+$FileName = Split-Path -Parent $scriptPath
 write-Host $FileName
 $FileTime = Get-Date
 
