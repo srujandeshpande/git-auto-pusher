85440299e33232ce781f2565d68ae71a2aab446b
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-13 23:10:00 +0530

    added new file for increment push

---
+++ time-based/bash-increment-git.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+clear
+echo "Welcome to git-auto-pusher! This was created by Srujan Deshpande"
+echo "Enter the duration in seconds you want to wait before commits"
+read duration
+
+git config --global credential.helper 'cache --timeout 10800'
+
+while true; do
+  sleep duration
+  git add .
+  git commit -m $date
+  git push
+done
